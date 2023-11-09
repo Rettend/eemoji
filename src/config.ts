b5696250efa08953536339b00f94a3b425f87a61
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-09 19:36:28 +0100

    ♻️ refactor: create utils

--- src/config.ts
+++ src/config.ts
@@ -8,13 +8,32 @@ export interface Config {
   }
 }
 
+export const configTypes = [
+  'ts',
+  'json',
+  'none',
+] as const
+
+export type ConfigType = typeof configTypes[number]
+export type JsFiles = typeof ConfigObject.prototype.jsFiles[number]
+export type JsonFiles = typeof ConfigObject.prototype.jsonFiles[number]
+
 export class ConfigObject {
   defaultConfig: Config = {
     format: '{emoji} {type}: {subject}',
     emojis,
   }
 
+  defaultTsConfig = `import { defineConfig } from 'eemoji'
+
+export default defineConfig({
+  format: '{emoji} {type}: {subject}', // this is the default format (you can remove it or change it)
+  emojis: {}, // this gets merged with the default emojis (use it to override emojis or add new ones)
+})
+`
+
   jsFiles = [
+    `${name}.config.ts`, // recommended
     `.${name}rc.js`,
     `.${name}rc.ts`,
     `.${name}rc.mjs`,
@@ -23,22 +42,21 @@ export class ConfigObject {
     `.config/${name}rc.ts`,
     `.config/${name}rc.cjs`,
     `${name}.config.js`,
-    `${name}.config.ts`,
     `${name}.config.mjs`,
     `${name}.config.cjs`,
-  ]
+  ] as const
 
   jsonFiles = [
+    `.${name}rc.json`, // recommended
     'package.json',
     `.${name}rc`,
-    `.${name}rc.json`,
     `.${name}rc.yaml`,
     `.${name}rc.yml`,
     `.config/${name}rc`,
     `.config/${name}rc.json`,
     `.config/${name}rc.yaml`,
     `.config/${name}rc.yml`,
-  ]
+  ] as const
 
   vscodeSettings = {
     'json.schemas': [
@@ -54,6 +72,7 @@ export class ConfigObject {
   vscodeSettingsFile = '.vscode/settings.json'
   hooksDir = '.git/hooks'
   hookFile = '.git/hooks/prepare-commit-msg'
+  gitCommitFile = '.git/COMMIT_EDITMSG'
 }
 
 export function defineConfig(config: Partial<Config>): Config {
---
+++ src/utils/emoji.ts
@@ -0,0 +1,62 @@
+import { consola } from 'consola'
+import { type Config } from './../config'
+
+export function eemojify(text: string, config: Config, DEBUG?: number): string {
+  // the separator is whatever character remains after removing the format placeholders, or a space
+  const separator = config.format.replace(/{emoji}|{type}|{subject}/g, '').trim() || ' '
+
+  const separatorIndex = text.indexOf(separator)
+  const type = text.substring(0, separatorIndex).trim()
+  const subject = text.substring(separatorIndex + separator.length).trim()
+
+  if (DEBUG && DEBUG >= 2)
+    consola.log(`\nconfig: ${JSON.stringify(config)}\n`)
+
+  if (DEBUG) {
+    consola.log(`separator: "${separator}"`)
+    consola.log(`type: "${type}"`)
+    consola.log(`subject: "${subject}"`)
+  }
+
+  if (!type || !subject)
+    throw new Error('Invalid commit message.')
+
+  let emoji: string | Record<string, string> | undefined
+
+  // if there is an exclamatory mark, then it's a breaking change
+  if (text.includes('!') && config.emojis.breaking)
+    emoji = config.emojis.breaking
+  else
+    emoji = config.emojis[type.toLowerCase().replace(/\(.*\)/, '')]
+
+  // if the emoji is an object, then it's a nested emoji
+  if (typeof emoji === 'object') {
+    emoji = getNestedEmoji(text, emoji)
+    consola.log(`nested emoji: "${emoji}"`)
+  }
+  else if (DEBUG) {
+    consola.log(`emoji: "${emoji}"`)
+  }
+
+  if (!emoji)
+    throw new Error(`Emoji for type "${type}" not found.`)
+
+  if (emoji.includes(','))
+    emoji = emoji.trim().split(',')[Math.floor(Math.random() * emoji.split(',').length)] ?? emoji
+
+  return config.format
+    .replace('{emoji}', emoji)
+    .replace('{type}', type)
+    .replace('{subject}', subject)
+}
+
+function getNestedEmoji(text: string, emoji: Record<string, string>): string | undefined {
+  const entries = Object.entries(emoji).filter(([key]) => key !== '.')
+
+  for (const [key, value] of entries) {
+    if (text.includes(key))
+      return value
+  }
+
+  return emoji['.']
+}
