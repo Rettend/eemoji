99872ce2cdbaa246186f8bb7bb34da50997db41e
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-11 12:30:48 +0100

    🚧 wip: still trying

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -81,7 +81,7 @@ function checkGitHook() {
   const sourceHookPath = path.join(__dirname, '..', '..', 'bin', 'hook.sh')
 
   if (!fs.existsSync(C.hooksDir)) {
-    fs.mkdirSync(C.hooksDir)
+    fs.mkdirSync(C.hooksDir, { recursive: true })
     consola.success('Created: .git/hooks')
   }
 
