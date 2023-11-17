40927ffab9300bf23de43f4b4289cfc8c0aa08c4
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-17 16:05:50 +0100

    🔧 fix: import

--- bin/eemoji.mjs
+++ bin/eemoji.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { process } from 'node:process'
-import { runMain } from '../dist/index.mjs'
+import { runMain } from '../src/index'
 
 if (!process.env.SKIP_POSTINSTALL)
   runMain()
