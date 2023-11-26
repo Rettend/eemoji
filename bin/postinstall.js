8753813c4d2c0f9c16abbda854d078c16d44ac6b
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-11-26 11:50:25 +0100

    💎 feat(enhance): add defaults to json schema

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,4 +1,5 @@
 const process = require('node:process')
+const fs = require('node:fs')
 const exec = require('node:child_process').exec
 const platform = require('node:os').platform()
 
@@ -22,3 +23,10 @@ else {
       console.log(stdout)
   })
 }
+
+const schema = require('./../json/eemoji-config-schema.json')
+const emojis = require('./../src/emojis.json')
+
+schema.properties.emojis.default = emojis
+
+fs.writeFileSync('./json/eemoji-config-schema.json', JSON.stringify(schema, null, 2))
