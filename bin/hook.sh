8685ed8de3205be215fe5768e2ff555ac890c085
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2024-01-20 22:15:25 +0100

    🚧 wip: test

--- bin/hook.sh
+++ bin/hook.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 LOCAL_PATH=$(npm root)
 NODE_PATH=$(npm root -g)
+REPO_ROOT=$(git rev-parse --show-toplevel)/node_modules
 
 if [ -f "$LOCAL_PATH/eemoji/bin/eemoji.mjs" ]; then
   node "$LOCAL_PATH/eemoji/bin/eemoji.mjs" run $1
 elif [ -f "$NODE_PATH/eemoji/bin/eemoji.mjs" ]; then
   node "$NODE_PATH/eemoji/bin/eemoji.mjs" run $1
+elif [ -f "$REPO_ROOT/eemoji/bin/eemoji.mjs" ]; then
+  node "$REPO_ROOT/eemoji/bin/eemoji.mjs" run $1
 else
   echo "eemoji is not properly installed locally or globally"
   exit 1
