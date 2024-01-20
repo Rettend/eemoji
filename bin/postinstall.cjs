729e81f37433adc790f90f93454c3247927f1284
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2024-01-20 20:47:11 +0100

    🐛 fix: bug with envs

--- bin/postinstall.cjs
+++ bin/postinstall.cjs
@@ -1,23 +1,20 @@
 const process = require('node:process')
 const fs = require('node:fs')
 const exec = require('node:child_process').exec
-const platform = require('node:os').platform()
 
-if (process.env.SKIP_POSTINSTALL) {
-  if (platform === 'linux') {
-    const schema = require('./../json/eemoji-config-schema.json')
-    const emojis = require('./../src/emojis.json')
+if (process.env.UPLOAD_SCHEMA) {
+  const schema = require('./../json/eemoji-config-schema.json')
+  const emojis = require('./../src/emojis.json')
 
-    schema.properties.emojis.default = emojis
+  schema.properties.emojis.default = emojis
 
-    fs.writeFileSync('./json/eemoji-config-schema.json', `${JSON.stringify(schema, null, 2)}\n`)
-  }
+  fs.writeFileSync('./json/eemoji-config-schema.json', `${JSON.stringify(schema, null, 2)}\n`)
 }
-else {
+else if (!process.env.SKIP_POSTINSTALL) {
   exec('node ./bin/eemoji.mjs init -c none', (err, stdout) => {
     if (err)
       console.error(err)
-    else
+    elsenode ./bin/eemoji.mjs init -c none
       console.log(stdout)
   })
 }
